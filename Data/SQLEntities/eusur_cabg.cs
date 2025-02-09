c8721b1c1edd4e869ddac12fcdb97073c300f47c
m_harder88@yahoo.com <m_harder88@yahoo.com>
2025-02-09 11:33:28 +0100

    eusur_cabg added

---
+++ Data/SQLEntities/eusur_cabg.cs
@@ -0,0 +1,75 @@
+﻿namespace SqlToMySql.Data.SqlEntities
+{
+    public class eusur_cabg
+    {
+        public int PROCEDURE_ID { get; set; }
+        public int PATIENT_ID { get; set; }
+        public int CAB { get; set; }
+        public int UNPLANNED_CAB { get; set; } = 0;
+        public int B1_SITE { get; set; }
+        public int B1_TYPE_DA { get; set; }
+        public int B1_LOCPROC { get; set; }
+        public int B1_CONDUIT { get; set; }
+        public int B1_SITEPA { get; set; }
+        public int B1_DONOR { get; set; }
+        public int B2_SITE { get; set; }
+        public int B2_TYPE_DA { get; set; }
+        public int B2_LOCPROC { get; set; }
+        public int B2_CONDUIT { get; set; }
+        public int B2_SITEPA { get; set; }
+        public int B2_DONOR { get; set; }
+        public int B3_SITE { get; set; }
+        public int B3_TYPE_DA { get; set; }
+        public int B3_LOCPROC { get; set; }
+        public int B3_CONDUIT { get; set; }
+        public int B3_SITEPA { get; set; }
+        public int B3_DONOR { get; set; }
+        public int B4_SITE { get; set; }
+        public int B4_TYPE_DA { get; set; }
+        public int B4_LOCPROC { get; set; }
+        public int B4_CONDUIT { get; set; }
+        public int B4_SITEPA { get; set; }
+        public int B4_DONOR { get; set; }
+        public int B5_SITE { get; set; }
+        public int B5_TYPE_DA { get; set; }
+        public int B5_LOCPROC { get; set; }
+        public int B5_CONDUIT { get; set; }
+        public int B5_SITEPA { get; set; }
+        public int B5_DONOR { get; set; }
+        public int B6_SITE { get; set; }
+        public int B6_TYPE_DA { get; set; }
+        public int B6_LOCPROC { get; set; }
+        public int B6_CONDUIT { get; set; }
+        public int B6_SITEPA { get; set; }
+        public int B6_DONOR { get; set; }
+        public string Q01 { get; set; }
+        public string Q02 { get; set; }
+        public string Q03 { get; set; }
+        public string Q04 { get; set; }
+        public string Q05 { get; set; }
+        public string Q06 { get; set; }
+        public string ANGLE01 { get; set; }
+        public string ANGLE02 { get; set; }
+        public string ANGLE03 { get; set; }
+        public string ANGLE04 { get; set; }
+        public string ANGLE05 { get; set; }
+        public string ANGLE06 { get; set; }
+        public string DIAM01 { get; set; }
+        public string DIAM02 { get; set; }
+        public string DIAM03 { get; set; }
+        public string DIAM04 { get; set; }
+        public string DIAM05 { get; set; }
+        public string DIAM06 { get; set; }
+        public string course { get; set; }
+        public string leg_harvest_location { get; set; }
+        public string radial_harvest_location { get; set; }
+        public string art01 { get; set; }
+        public string art02 { get; set; }
+        public string art03 { get; set; }
+        public string art04 { get; set; }
+        public string art05 { get; set; }
+        public string art06 { get; set; }
+        public int tachtig_switch { get; set; }
+    }
+}
+
