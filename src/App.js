947bd62a734dc13b5d783e54a57eac7586cf06f0
marcel <marcelharder@protonmail.com>
2025-03-02 13:46:50 +0100

    nummer1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
+import React from 'react';
+import {
+  MDBContainer, MDBInput, MDBCheckbox, MDBBtn, MDBIcon
+}
+from 'mdb-react-ui-kit';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+   
+   <><div class="Row">
+      <div class="col-md-6">hier komt de side bar</div>
+      <div class="col-md-6"><MDBContainer className="p-3 my-5 d-flex flex-column w-50">
+
+<MDBInput wrapperClass='mb-4' label='Email address' id='form1' type='email' />
+<MDBInput wrapperClass='mb-4' label='Password' id='form2' type='password' />
+
+<div className="d-flex justify-content-between mx-3 mb-4">
+  <MDBCheckbox name='flexCheck' value='' id='flexCheckDefault' label='Remember me' />
+  <a href="#!">Forgot password?</a>
+</div>
+
+<MDBBtn className="mb-4">Sign in (click)="login()"</MDBBtn>
+
+<div className="text-center">
+  <p>Not a member? <a href="#!">Register</a></p>
+  <p>or sign up with:</p>
+
+  <div className='d-flex justify-content-between mx-auto icon-container'>
+    <MDBBtn tag='a' color='none' className='m-1' style={{ color: '#1266f1' }}>
+      <MDBIcon fab icon='facebook-f' size="sm" />
+    </MDBBtn>
+
+    <MDBBtn tag='a' color='none' className='m-1' style={{ color: '#1266f1' }}>
+      <MDBIcon fab icon='twitter' size="sm" />
+    </MDBBtn>
+
+    <MDBBtn tag='a' color='none' className='m-1' style={{ color: '#1266f1' }}>
+      <MDBIcon fab icon='google' size="sm" />
+    </MDBBtn>
+
+    <MDBBtn tag='a' color='none' className='m-1' style={{ color: '#1266f1' }}>
+      <MDBIcon fab icon='github' size="sm" />
+    </MDBBtn>
+
+  </div>
+</div>
+
+</MDBContainer></div>
     </div>
+    
+    
+      
+      
+      </>
   );
 }
 
